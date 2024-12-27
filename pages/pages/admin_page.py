9b3d123d4c5e74f597e0ae5e885263d56a66aedf
Pavel Falin <165943002+falinpavel@users.noreply.github.com>
2024-12-27 16:45:28 +0300

    started new tests

--- pages/pages/admin_page.py
+++ pages/pages/admin_page.py
@@ -9,15 +9,34 @@ from selenium.webdriver.support import expected_conditions as EC
 class AdminPage(BasePage):
     PAGE_URL = Links.ADMIN_PAGE
 
+    # for page add (postfix ADD)
     LINK_ADMIN = ("xpath", "(//ul[@class= 'oxd-main-menu']/li)[1]")
-    USERNAME_TEXT = ("xpath", "//label[normalize-space()='Username']")
-    USERNAME_FIELD = ("xpath", "(//input)[2]")
+    USER_BUTTON_ADD = ("xpath", "//button[@class='oxd-button oxd-button--medium oxd-button--secondary']")
+    USERNAME_TEXT_ADD = ("xpath", "//label[normalize-space()='Username']")
+    EMPLOYEE_NAME_FIELD_ADD = ("xpath", "(//input)[2]")
+    USERNAME_FIELD_ADD = ("xpath", "(//input)[3]")
+    PASSWORD_TEXT_ADD = ("xpath", "//label[normalize-space()='Password']")
+    PASSWORD_FIELD_ADD = ("xpath", "(//input)[4]")
+    CONFIRM_PASSWORD_TEXT_ADD = ("xpath", "//label[normalize-space()='Confirm Password']")
+    CONFIRM_PASSWORD_FIELD_ADD = ("xpath", "(//input)[5]")
+    HINT_TEXT_ADD = ("xpath", "//p[@class='oxd-text oxd-text--p user-password-hint']") # For a strong password, please use a hard to guess combination of text with upper and lower case characters, symbols and numbers
+    HINT_REQ_USER_ROLE_ADD = ("xpath", "(//span[text()='Required'])[1]")
+    HINT_REQ_EMPLOYEE_NAME_ADD = ("xpath", "(//span[text()='Required'])[2]")
+    HINT_REQ_STATUS_ADD = ("xpath", "(//span[text()='Required'])[3]")
+    HINT_REQ_USERNAME_ADD = ("xpath", "(//span[text()='Required'])[4]")
+    HINT_REQ_PASSWORD_ADD = ("xpath", "(//span[text()='Required'])[5]")
+    HINT_REQ_CONFIRM_PASSWORD_ADD = ("xpath", "//span[text()='Passwords do not match']")
+    BUTTON_CANCEL_ADD = ("xpath", "(//button)[4]")
+    BUTTON_SAVE_ADD = ("xpath", "(//button)[5]")
+    # for page search
+    USERNAME_TEXT_SEARCH = ("xpath", "//label[normalize-space()='Username']")
+    USERNAME_FIELD_SEARCH = ("xpath", "(//input)[2]")
     USER_ROLE_DROPDOWN = ("xpath", "(//div[@class='oxd-select-text oxd-select-text--active'])[1]")
     USER_ROLE_TEXT = ("xpath", "//label[normalize-space()='User Role']")
     USER_ROLE_ADMIN = ("xpath", "//div[@role='option']/span[text()='Admin']")
     USER_ROLE_ESS = ("xpath", "//div[@role='option']/span[text()='ESS']")
     EMPLOYEE_NAME_TEXT = ("xpath", "//label[normalize-space()='Employee Name']")
-    EMPLOYEE_NAME_FIELD = ("xpath", "(//input)[3]")
+    EMPLOYEE_NAME_FIELD_SEARCH = ("xpath", "(//input)[3]")
     STATUS_TEXT = ("xpath", "//label[normalize-space()='Status']")
     STATUS_DROPDOWN = ("xpath", "//div[@class='oxd-select-text oxd-select-text--focus']")
     STATUS_DROPDOWN_ENABLED = ("xpath", "//div[@role='option']/span[text()='Enabled']")
@@ -29,14 +48,22 @@ class AdminPage(BasePage):
                 EC.element_to_be_clickable(self.LINK_ADMIN)
             ).click()
 
-    def add_username_field(self, username):
+    def click_add_button(self):
+        with allure.step("Click on 'Add' button"):
+            self.wait.until(
+                EC.element_to_be_clickable(self.USER_BUTTON_ADD)
+            ).click()
+            time.sleep(1)
+            assert self.driver.current_url == "https://opensource-demo.orangehrmlive.com/web/index.php/admin/saveSystemUser" # TODO fix
+
+    def add_username_field(self, username): # TODO back to work later
         with allure.step("Enter username"):
             name_field = self.wait.until(
-                EC.element_to_be_clickable(self.USERNAME_TEXT)
+                EC.element_to_be_clickable(self.USERNAME_TEXT_SEARCH)
             ).text
             assert name_field == 'Username'
             new_value = self.wait.until(
-                EC.element_to_be_clickable(self.USERNAME_FIELD)
+                EC.element_to_be_clickable(self.USERNAME_FIELD_SEARCH)
             )
             new_value.click()
             new_value.send_keys(Keys.CONTROL + "a")
@@ -44,7 +71,7 @@ class AdminPage(BasePage):
             new_value.send_keys(username)
             assert new_value.get_attribute("value") == username
 
-    def add_user_role_dropdown(self, user_role):
+    def add_user_role_dropdown(self, user_role): # TODO back to work later
         with allure.step("Click on 'User Role' dropdown"):
             name_field = self.wait.until(
                 EC.element_to_be_clickable(self.USER_ROLE_TEXT)
@@ -72,14 +99,14 @@ class AdminPage(BasePage):
             else:
                 raise Exception("Can't click on 'User Role' dropdown")
 
-    def add_employee_name_field(self, employee_name):
+    def add_employee_name_field(self, employee_name): # TODO back to work later
         with allure.step("Enter employee name"):
             name_field = self.wait.until(
                 EC.element_to_be_clickable(self.EMPLOYEE_NAME_TEXT)
             ).text
             assert name_field == 'Employee Name'
             new_value = self.wait.until(
-                EC.element_to_be_clickable(self.EMPLOYEE_NAME_FIELD)
+                EC.element_to_be_clickable(self.EMPLOYEE_NAME_FIELD_SEARCH)
             )
             new_value.click()
             new_value.send_keys(Keys.CONTROL + "a")
@@ -87,7 +114,7 @@ class AdminPage(BasePage):
             new_value.send_keys(employee_name)
             assert new_value.get_attribute("value") == employee_name
 
-    def add_status_dropdown(self, status):
+    def add_status_dropdown(self, status): # TODO back to work later
         with allure.step("Click on 'Status' dropdown"):
             name_field = self.wait.until(
                 EC.element_to_be_clickable(self.STATUS_TEXT)
--- tests/pages/test_admin_page.py
+++ tests/pages/test_admin_page.py
@@ -6,6 +6,38 @@ import utils.faker_data as FD
 
 @allure.feature("Admin page tests")
 class TestAdminPage(BaseTest):
+
+    @allure.title("Add user admin")
+    @allure.severity("Critical")
+    @pytest.mark.smoke
+    def test_add_new_user_admin(self):
+        """
+        Тест-кейс: Проверка страницы администратора
+        1. Открыть главную страницу
+        2. Ввести валидный логин
+        3. Ввести валидный пароль
+        4. Нажать на кнопку "Submit"
+        5. Нажать на ссылку "Admin"
+        6. Проверить, что открылась страница администратора
+        7. Нажать на кнопку "Add"
+        8. В разделе "Add User" в выпадающем списке "User Role" выбрать "Admin"
+        9. В разделе "Add User" в поле ввода "Employee Name" ввести валидное значение
+        10. В разделе "Add User" в выпадающем списке "Status" выбрать "Enabled"
+        11. Нажать на кнопку "Save"
+        12. Проверить, что открылась страница администратора
+        13. Нажать на ссылку "Admin"
+        14. Проверить, что открылась страница администратора
+        15. Нажать на ссылку "Logout"
+        16. Проверить, что открылась главная страница
+        """
+        self.login_page.open()
+        self.login_page.enter_login(self.data.LOGIN)
+        self.login_page.enter_password(self.data.PASSWORD)
+        self.login_page.click_submit_button()
+        self.admin_page.click_admin_link()
+        self.admin_page.click_add_button()
+        self.admin_page.add_user_role_dropdown("Admin")
+
     @allure.title("Check admin page and create user admin, status = enabled")
     @allure.severity("Major")
     @pytest.mark.smoke
